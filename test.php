8f0a6144452da9f1701f991054901c9216f11f26
Sumanth Neerumalla <sumanth42gold@gmail.com>
2015-08-03 12:43:38 -0400

    a cursory look of the pages shows that the length of the html code is always the same and the titles of each page as well as the comic source are both always in the same location. I should be able to move over the code in test.php over to the main code and stop using the strtok function with such fruitless results

--- test.php
+++ test.php
@@ -1,13 +1,18 @@
 <?php
-$webPage = file_get_contents("http://www.commitstrip.com/en/page/691/");
+$webPage = file_get_contents("http://www.commitstrip.com/en/page/692/");
 $count = 0;
 $webPage = explode ("\n",$webPage );
 
 foreach ($webPage as $line)
 {
     $count = $count + 1;
-    #$characterArray = htmlentities($line,ENT_QUOTES);
-    echo $webPage[19];
+
+    if ($count == 213) {
+        $imagePortion = htmlentities($line,ENT_QUOTES);
+    }
+    if ($count == 29) {
+        $titlePortion = htmlentities($line,ENT_QUOTES);
+    }
 
 }
 echo $count;
